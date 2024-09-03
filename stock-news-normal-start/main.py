98b592a7d51819a4d639d2435ad8d5edd5c8cc55
NATHALIE S. <160782031+Nathalie733@users.noreply.github.com>
2024-09-03 15:16:23 -0300

    Add files via upload

---
+++ stock-news-normal-start/main.py
@@ -0,0 +1,62 @@
+import requests
+from twilio.rest import Client
+
+STOCK_NAME = "TSLA"
+COMPANY_NAME = "Tesla Inc"
+
+STOCK_ENDPOINT = "https://www.alphavantage.co/query"
+NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+TWILIO_SID = "AC5d336f5f3a59f2b893b52bfbd2096cb0"
+TWILIO_AUTH_TOKEN = "3535bc830edb95cd20029e0514647b49"
+
+STOCK_API_KEY = "X41W04MF4J68SI6K"
+NEWS_API_KEY = "9f8379204126422797bdde9fb2555d28"
+stock_params = {
+    "function": "TIME_SERIES_DAILY",
+    "symbol": STOCK_NAME,
+    "apikey": STOCK_API_KEY,
+}
+response = requests.get(STOCK_ENDPOINT, params=stock_params )
+data = response.json()["Time Series (Daily)"]
+data_list = [value for (key, value) in data.items()]
+yesterday_data = data_list[0]
+yesterday_closing_price = yesterday_data["4. close"]
+print(yesterday_closing_price)
+
+day_before_yesterday_data = data_list[1]
+day_before_yesterday_closing_price = day_before_yesterday_data["4. close"]
+print(day_before_yesterday_closing_price)
+
+difference = float(yesterday_closing_price) - float(day_before_yesterday_closing_price)
+up_down = None
+if difference > 0:
+    up_down = "🔺"
+else:
+    up_down = "🔻"
+
+diff_percent =round((difference / float(yesterday_closing_price)) * 100
+print(diff_percent)
+
+if abs(diff_percent) > 1:
+    news_params = {
+        "apiKey": NEWS_API_KEY,
+        "qInTitle": COMPANY_NAME,
+
+    }
+    news_response = requests.get(NEWS_ENDPOINT, params=news_params)
+    articles = news_response.json()["articles"]
+    print(articles)
+
+    three_articles = articles[:3]
+    print(three_articles)
+
+    formatted_articles = [f"{STOCK_NAME}: {up_down}{diff_percent}%\nHeadline: {article['title']}. \nBrief: {article ['description']}" for article in three_articles]
+
+        Client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+
+    for article in formatted_articles:
+        message = client.messages.create(
+            body=article,
+            from= "+15167306982",
+            to= "",
+        )
